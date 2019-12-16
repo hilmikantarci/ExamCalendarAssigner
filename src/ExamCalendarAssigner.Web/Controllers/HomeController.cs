eaddf77f2e7bc75b014a4f6e58f1799f7bde7d1e
Hilmi Kantarci <hilmikantarci@gmail.com>
2019-12-17 01:48:13 +0300

    UploadJsonFile

--- src/ExamCalendarAssigner.Web/Controllers/HomeController.cs
+++ src/ExamCalendarAssigner.Web/Controllers/HomeController.cs
@@ -8,6 +8,10 @@ using Microsoft.Extensions.Logging;
 using ExamCalendarAssigner.Web.Models;
 using ExamCalendarAssigner.Data.Model;
 using Newtonsoft.Json;
+using System.IO;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+
 
 namespace ExamCalendarAssigner.Web.Controllers
 {
@@ -23,9 +27,56 @@ namespace ExamCalendarAssigner.Web.Controllers
         public IActionResult Index()
         {
             string json = System.IO.File.ReadAllText(@"C:\Users\Hilmi\Desktop\data.json");
-            SınavTakvimModel model =JsonConvert.DeserializeObject<SınavTakvimModel>(json);
+            SınavTakvimModel model = JsonConvert.DeserializeObject<SınavTakvimModel>(json);
             return View();
         }
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Upload(IFormFile files)
+        {
+
+            var content = string.Empty;
+
+            using (var reader = new StreamReader(files.OpenReadStream()))
+            {
+                content = reader.ReadToEnd();
+            }
+       
+            try
+            {
+                SınavTakvimModel model = JsonConvert.DeserializeObject<SınavTakvimModel>(content);                         
+                
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+          
+            
+
+            return Ok();
+        }
+        //public async Task<IActionResult> Upload(List<IFormFile> files)
+        //{
+        //    //files.SaveAs(Server.MapPath())
+
+        //    //return View("index");
+        //    foreach (var file in files)
+        //    {
+        //        if (file.ContentLength > 0)
+        //        {
+        //            var fileName = Path.GetFileName(file.FileName);
+        //            var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+        //            file.SaveAs(path);
+        //        }
+        //    }
+        //    return RedirectToAction("Index");
+        //}
 
         public IActionResult Privacy()
         {
