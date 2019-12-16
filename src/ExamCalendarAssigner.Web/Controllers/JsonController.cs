49dfee787d5cd44afc6156784608f2ef77087f9d
Hilmi Kantarci <hilmikantarci@gmail.com>
2019-12-16 03:58:16 +0300

    deneme

---
+++ src/ExamCalendarAssigner.Web/Controllers/JsonController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamCalendarAssigner.Web.Controllers
+{
+    public class JsonController:Controller
+    {
+        public IActionResult FileJson()
+        {
+            return View();
+        }
+    }
+}
